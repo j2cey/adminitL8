9b8dd6b31c2d75561364db4da7b54015708a3268
j2cey <jud10parfait@gmail.com>
2020-11-02 14:19:11 +0000

    Base Model & Migration + ADLDAP

---
+++ app/Models/FonctionEmploye.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FonctionEmploye extends Model
+{
+    use HasFactory;
+}
